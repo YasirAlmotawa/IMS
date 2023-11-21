d31932837322b1666b23ec715b09c54251986ebf
YasirAlmotawa <yalmotawa2@huskers.unl.edu>
2023-11-21 17:41:31 -0600

    learning more js

---
+++ javascript/jsPractice.js
@@ -0,0 +1,37 @@
+
+//let name = 'Yasir';
+//let age = 10
+
+let person = {
+    name: 'Yasir',
+    age: 19
+};
+
+//dot notation
+person.name = 'John';
+
+//bracket noration
+person['name'] = 'Mary';
+
+//arrays
+let selectedColors = ['red', 'blue', 'brown'];
+
+function greet(yourName){
+    console.log('Hello ' + yourName);
+}
+greet('mo');
+
+
+
+//typeof gets the type of the variable
+//let declares the variable
+//const makes a constant variable
+//A variable can assigned to number, boolean, undefined and null
+// "Adding a quotation mark like this \"\" "
+// You can also use single quote just to pring out a ""
+//arrayName.push(whatever you want to add to array)
+//arrayName.pop()
+//arrayName.shift()
+//arrayName.ushift()
+//3 == '3'
+//3 !=== '3'
